dfa90944865be2170c3636191b29f6a81fb224b0
Victoria <victoria.perello@mercadolibre.com>
2014-11-05 09:34:14 -0300

    inicial

---
+++ Test/HelloWord/src/HelloWord.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWord.cpp
+// Author      : Vicky
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World Nico Lindo!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Testing!!!" << endl;
+	cout << "!!!Testing nuvamente!!!" << endl;
+	return 0;
